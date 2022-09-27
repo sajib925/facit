fba8e15dd3379384740156bdfeddc9a1528f6f11
sajib925 <sajibahmed1004@gmail.com>
2022-09-27 19:46:49 +0600

    main file update

--- src/main.jsx
+++ src/main.jsx
@@ -13,8 +13,8 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />}>
-          <Route index element={<Dashboard />} />
-          <Route path="login" element={<Login />} />
+          <Route index element={<Login />} />
+          <Route path="dashboard" element={<Dashboard />} />
           <Route path="signUp" element={<SignUp />} />
         </Route>
       </Routes>
